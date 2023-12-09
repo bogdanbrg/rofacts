86a4042a3564a65a55414cda804a54690f0327bf
bogdan <bogdanbirgovan@gmail.com>
2023-12-09 23:20:40 +0100

    comment matplotlib

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 #import plotly.express as px
 import pandas as pd
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 import wbgapi as wb
 
