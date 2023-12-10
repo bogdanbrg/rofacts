5af7dbd4ce756a37fb30cb3e6bd446f548a2e28d
bogdan <bogdanbirgovan@gmail.com>
2023-12-10 14:37:37 +0100

    import csv file from local instead of wgbi API

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,31 +1,15 @@
 import streamlit as st
 import plotly.express as px
 import pandas as pd
-#import matplotlib.pyplot as plt
 import plotly.graph_objects as go
-import wbgapi as wb
 
-st.set_page_config(page_title = "Superstore!!!", page_icon = ":bar_chart:", layout="wide")
+
+st.set_page_config(page_title = "Rofacts", page_icon = ":bar_chart:", layout="wide")
 
 st.title(":bar_chart: Romania indicators")
 
-     
-df_unemployment =wb.data.DataFrame('SL.UEM.TOTL.NE.ZS', 'ROU', range(1994,2023))
-# transpose the data
 
-# Transpose DataFrame while keeping the index as columns
-df_unemployment_transposed = df_unemployment.T.reset_index()
-
-# Rename the columns
-df_unemployment_transposed.columns = ['year', 'unemployment rate']
-
-# Print the transposed DataFrame
-
-df_unemployment_transposed['year'] = df_unemployment_transposed['year'].str.replace('YR', '')
-
-# Define background color
-
-df = df_unemployment_transposed
+df = pd.read_csv(r'C:\Users\bogda\OneDrive\Data\ro_unemployment.csv')
 color_background = 'rgb(253,241,230)'
 
 fig = go.Figure()
