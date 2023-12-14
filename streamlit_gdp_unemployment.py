58caa59733c85642e8fca5663f2fa4a036aa5e9e
bogdan <bogdanbirgovan@gmail.com>
2023-12-14 20:14:14 +0100

    add 3rd graph

--- streamlit_gdp_unemployment.py
+++ streamlit_gdp_unemployment.py
@@ -86,18 +86,55 @@ fig_gdp.update_layout(
     paper_bgcolor=color_background
 )
 
-# Show the plot
-#fig_gdp.show()
+############## INFLATION #################
+
+
+df_inflation =wb.data.DataFrame('FP.CPI.TOTL.ZG', 'ROU', range(1991,2023))
+
+# transpose the data
+
+# Transpose DataFrame while keeping the index as columns
+df_inflation_transposed = df_inflation.T.reset_index()
+
+# Rename the columns
+df_inflation_transposed.columns = ['year', 'inflation %']
+
+df_inflation_transposed['year'] = df_inflation_transposed['year'].str.replace('YR', '')
+
+
+# Define background color
+
+df_inflation = df_inflation_transposed
+color_background = 'rgb(253,241,230)'
+
+fig_inflation = go.Figure()
+
+# Add a line trace for GDP
+fig_inflation.add_trace(go.Scatter(x=df_inflation['year'], y=df_inflation['inflation %'],
+                         mode='lines+markers',
+                         name='inflation (%)'))
+
+# Update layout
+fig_inflation.update_layout(
+    title='Inflation Evolution Over the Years',
+    xaxis_title='Year',
+    yaxis_title='Inflation (%)',
+    plot_bgcolor=color_background,
+    paper_bgcolor=color_background
+)
 
 # Show the plot
 
 #st.plotly_chart(fig_gdp)
 
-cc1,cc2 = st.columns([1,1])
+col1,col2 = st.columns(2, gap = "medium")
 
-with cc1:
+with col1:
     st.plotly_chart(fig_gdp)
     st.plotly_chart(fig)
+    
+with col2:
+    st.plotly_chart(fig_inflation)
 
 
 
